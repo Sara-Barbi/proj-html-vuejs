b4c1c926df2c3bd9e062a145e119241a9ad5497d
Sara-Barbi <barbisara02@gmail.com>
2022-02-08 18:18:02 +0100

    prima parte main

--- src/components/MAIN.vue
+++ src/components/MAIN.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="main">
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-6  ps-5 ">
+          <p>EVERYTHING IN MAXCOACH</p>
+          <h2>Learn about our <h2 class="thinH2">Work Culture</h2>at maxCoach</h2>
+          <p>Spend some time to visit our website ar head office and discover our current courses, enrollmen procedure,and regustration deadline. Were opening new classes every beginning of each month.</p>
+          <p>Have questions?get Free Guide</p>
+        </div>
+        <div class="col-6">
+          <img class='imgPrinc' src="../assets/image/stock-full-hd-10-ohki1ayywq3cqutettrncduujpoaorifqu4relt2ts.jpg" alt="">
+          <img class='imgTop' src="../assets/image/about-us-01-ohkia3qg98rq9vg3s0mn2hkflr1migdqdw370qnegc.jpg" alt="">
+          <img class='pattern' src="../assets/image/maxcoach-shape-07-100x100.png" alt="">
+          <img class='circle' src="../assets/image/maxcoach-shape-02.png" alt="">
+
+
+        </div>
+      </div>
+    </div>
 
     <courses/>
 
@@ -29,6 +47,42 @@ export default {
 <style scoped lang="scss">
   @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"); 
   @import '../assets/style/variables.scss';
+  div{
+    
+    position: relative;
+    .imgPrinc{
+      width: 300px;
+      position: relative;
+      top: 50px;
+      right: -40px;
+      margin: auto 0;
+      z-index: 1;
+
+    }
+    .imgTop{
+      position: absolute;
+      width: 80px;
+      top: 20px;
+      z-index: 2;
+    
+
+    }
+    .pattern{
+      position: absolute;
+      top: 26px;
+      right: 210px;
+      width: 78px;
+
+    }
+    .circle{
+      width:70px;
+      position: absolute;
+      z-index: 3;
+      bottom: 10px;
+      right: 250px;
+
+    }
+  }
   
 
 </style>

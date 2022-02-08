edd2145763d871500333e55f2e64e2b0bf512a37
Sara-Barbi <barbisara02@gmail.com>
2022-02-08 17:24:44 +0100

    HEADER

--- src/components/HEADER.vue
+++ src/components/HEADER.vue
@@ -3,6 +3,15 @@
     <NAVbar 
         :arr='navArr'
         />
+    <div class="container-fluid ">
+      <div class="row">
+        <div class="col-12 text-center d-flex flex-column justify-content-center align-items-center myBack">
+          <h3>Commence Buisiness</h3>
+          <h1>Distant Mentoring Program</h1>
+          <button class="downloadGuide">Download free guidebook <i class="bi bi-arrow-right"></i></button>
+        </div>
+      </div>
+    </div>
       
   </div>
 </template>
@@ -45,6 +54,23 @@ export default {
 <style scoped lang="scss">
   @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"); 
   @import '../assets/style/variables.scss';
-  
+  .myBack{
+    background-image: $imgBack;
+    background-size: $sizeBack;
+    height: $backHeight;
+    color: white;
+
+  }
+  .downloadGuide{
+    width: 18%;
+    background-color: white;
+    font-size: 13px;
+    font-weight: bold;
+    color: rgb(27, 144, 105);
+    padding: 10px 0;
+    border: 0px;
+    border-radius: 10px;
+  }
+
 
 </style>
